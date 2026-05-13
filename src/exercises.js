3d847f7ba8951c9cc7b153c6d99182497c138ea6
Magali Suarez <marianajunin11@gmail.com>
2026-05-13 11:20:00 -0300

    feat: add second array exercise

--- src/exercises.js
+++ src/exercises.js
@@ -2,3 +2,11 @@ function removeFirstElement(array){
     //usamos slice() para NO modificar el array original y no shift()
     return array.slice(1);
 }
+
+function removeLastThreeIfValid(array){
+    if (array.length <= 5){
+        return 'ERROR: el arreglo debe tener mas de 5 elementos';
+    }
+//usamos slice(0,-3) porque devuelve UN NUEVO array sin los ultimos 3 elemntos, SIN modificar el original
+    return array.slice(0,-3);
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+//VAMOS CON EL EJERCICIO 1 :D
 const buttonExercise1 = document.querySelector('#btn-exercise-1');
 const resultExercise1 = document.querySelector('#result-1');
 
@@ -6,4 +7,23 @@ buttonExercise1.addEventListener('click', () => {
     const result = removeFirstElement(input);
 
     resultExercise1.textContent = JSON.stringify(result);
+});
+
+//VAMOS CON EL EJERCICIO 2
+const buttonExercise2Valid = document.querySelector('#btn-exercise-2-valid');
+const buttonExercise2Invalid = document.querySelector('#btn-exercise-2-invalid');
+const resultExercise2 = document.querySelector('#result-2');
+
+buttonExercise2Valid.addEventListener('click', () => {
+    const input = [1, 2, 3, 4, 5, 6, 7];
+    const result = removeLastThreeIfValid(input);
+
+    resultExercise2.textContent = JSON.stringify(result);
+});
+
+buttonExercise2Invalid.addEventListener('click', () => {
+    const input = [1, 2, 3, 4, 5];
+    const result = removeLastThreeIfValid(input);
+
+    resultExercise2.textContent = result;
 });
